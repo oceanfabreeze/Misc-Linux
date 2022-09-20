0db30fc8856cfe5a52618120defcb3fc5bae7500
Thomas A. Fabrizio <me@thomasfabriziojr.com>
2022-09-19 23:34:14 -0500

    Modify ipmitemp.sh and add readme.sh
    Added a readme and modified ipmitemp.sh to add elif statement to restore fan control.

--- dell-fancontrol/ipmitemp.sh
+++ dell-fancontrol/ipmitemp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # -------------------------------------------------------------------------
 #author=Thomas A. Fabrizio (TF054451)
-#verison=1.0
-#Changelog= First commit.
+#verison=1.1
+#Changelog= added some elif and changed the variables
 # ----------------------------------------------------------------------------------
 # Script for checking the temperature that's reported by the ambient temperature sensor,
 # and if it's too high send the raw IPMI command to enable dynamic fan control.
@@ -23,24 +23,21 @@ IPMIPW=calvin
 # Change this to the temperature in celcius you are comfortable with.
 # If the temperature goes above the set degrees it will send raw IPMI command to enable dynamic fan control
 MAXTEMP=30
+SAFETEMP=29
 
 # This variable sends a IPMI command to get the ambient temp, and outputs it.
 TEMP=$(ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW sdr type temperature |grep Ambient |grep degrees |grep -Po '\d{2}' | tail -1)
 
 
-if [[ $TEMP > $MAXTEMP ]];
+if [ $TEMP -ge $MAXTEMP ];
     then
         printf "\nWARNING: Temperature is too high! Activating dynamic fan control! ($TEMP C)" >> temperaturealerts.log
         ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x01
-    else
-        printf "\nTemperature is OK ($TEMP C)" >> temperaturealerts.log
-fi
-
-if [[ $TEMP < $MAXTEMP ]];
+elif [ $TEMP -eq $SAFETEMP ]
     then
-        printf "\nALERT: Temperature has been restored to safe levels. ($TEMP C)" >> temperaturealerts.log
-        ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
-        ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x12
-    else
-        printf "\nTemperature is still too high! ($TEMP C)" >> temperaturealerts.log
+        printf "\nALERT:Temperature is elevated ($TEMP C)" >> temperaturealerts.log
+else
+    printf "\nMESSAGE:Temperature is at safe levels. ($TEMP C)" >> temperaturealerts.log
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x01 0x00
+    ipmitool -I lanplus -H $IPMIHOST -U $IPMIUSER -P $IPMIPW raw 0x30 0x30 0x02 0xff 0x12
 fi
