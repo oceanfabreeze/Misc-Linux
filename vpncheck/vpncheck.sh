f2922718bedd4d239adcde522f122d2d42ae1bc1
Thomas A. Fabrizio <me@thomasfabriziojr.com>
2023-04-26 18:39:03 -0400

    Update vpncheck.sh
    Updating vpncheck so logging includes date.

--- vpncheck/vpncheck.sh
+++ vpncheck/vpncheck.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 # -------------------------------------------------------------------------
 #author=Thomas A. Fabrizio (TAFFY)
-#verison=1.0
-#Changelog=
+#verison=1.1
+#Changelog=Adding Date to logfile
 # ----------------------------------------------------------------------------------
 # Script to check if openvpn is running. If not, start it. 
 # ----------------------------------------------------------------------------------
+
+$DATE = date;
+
 if pgrep openvpn;
     then
-        printf "\nVPN IS RUNNING!" >> vpn.log
+        printf "\n$DATE VPN IS RUNNING!" >> vpn.log
 else
-    printf "\nVPN IS NOT RUNNING. STARTING." >> vpn.log
+    printf "\n$DATE VPN IS NOT RUNNING. STARTING." >> vpn.log
     service openvpn-client@vpn start
 fi
